fba97f4e13fc20312528fcb0ef3beb34c47f7405
mbiselx <michael.biselx@epfl.ch>
2023-08-08 15:06:12 +0200

    updated and corrected examples

--- examples/read_ethernet.py
+++ examples/read_ethernet.py
@@ -1,9 +1,61 @@
 from rga_whisperer.gui import Plotter, mkQApp
 from rga_whisperer.interfaces import ethernet
-from rga_whisperer.message_types import *
+from rga_whisperer.message_types import (
+    parse_message,
+    DataStream1Message, DataStream2Message,
+    BindResponseMessage, UK_Request_1_Message,
+)
 
+HOST = ...
+PORT = 56765
 
 app = mkQApp('Ethernet Live Feed')
 gui = Plotter()
 
-app.exec()
+# this is the COM object reference we need to look for
+object_ref = -1
+# this is the data item tag we need to look for
+item_handle = -1
+
+
+def callback(data):
+    '''this function called for every COM data packet'''
+    # use global variables because *that* never causes problems...
+    global object_ref, item_handle
+
+    msg = parse_message(data)
+
+    # in the initial state, we don't know the object reference
+    if isinstance(msg, (BindResponseMessage)):
+        _, object_ref_str, _ = msg.bind_request.split('_')
+        object_ref = int(object_ref_str, 16)
+        print(f"{object_ref=}")
+    elif object_ref < 0:
+        return
+
+    # once we kow the object ref, we can try to find the data item handle
+    if isinstance(msg, UK_Request_1_Message):
+        for item in msg.items:
+            if item.name == 'Channels.Actuality.MeasureValue':
+                item_handle = item.ref
+                break
+        print(f"{item_handle=}")
+    elif item_handle < 0:
+        return
+
+    if isinstance(msg, (DataStream1Message, DataStream2Message)) and \
+            msg.header.object_ref == object_ref:
+        for e in msg.elements:
+            if e.item_handle == item_handle and isinstance(e.value, list):
+                gui.set_ydata(e.value)
+                break
+
+
+# the main program loop
+with ethernet.TCPSniffer(HOST, PORT, callback) as sniffer:
+    # start the GUI
+    gui.show()
+
+    # this is the event loop, which is responsible for the GUI.
+    # it is run in the main thread
+    app.exec()
--- examples/read_pcap.py
+++ examples/read_pcap.py
@@ -3,36 +3,67 @@ from threading import Thread
 
 from rga_whisperer.interfaces import pcap
 from rga_whisperer.gui import Plotter, mkQApp
-from rga_whisperer.message_types import *
+from rga_whisperer.message_types import (
+    parse_message,
+    DataStream1Message, DataStream2Message,
+    BindResponseMessage, UK_Request_1_Message,
+)
 
 filename = ...
 
-app = mkQApp('Ethernet Live Feed')
+app = mkQApp('PCAP Playback')
 gui = Plotter()
 
 
 def run():
     '''this function is run by the thread, to prevent blocking the GUI'''
+
+    # this is the COM object reference we need to look for
+    object_ref = -1
+    # this is the data item tag we need to look for
+    item_handle = -1
+
     for _, data in pcap.read_pcap(filename):
         msg = parse_message(data)
 
-        if isinstance(msg, (DataStream1Message, DataStream2Message)):
-            if msg.header.object_ref == 120827608:
-                for e in msg.elements:
-                    if e.item_handle == 116579336 and isinstance(e.value, list):
-                        gui.set_ydata(e.value)
-                        break
-                time.sleep(.5)
+        # in the initial state, we don't know the object reference
+        if isinstance(msg, (BindResponseMessage)):
+            _, object_ref_str, _ = msg.bind_request.split('_')
+            object_ref = int(object_ref_str, 16)
+            print(f"{object_ref=}")
+        elif object_ref < 0:
+            continue
+
+        # once we kow the object ref, we can try to find the data item handle
+        if isinstance(msg, UK_Request_1_Message):
+            for item in msg.items:
+                if item.name == 'Channels.Actuality.MeasureValue':
+                    item_handle = item.ref
+                    break
+            print(f"{item_handle=}")
+        elif item_handle < 0:
+            continue
+
+        # only data stream messages will be of any use
+        if isinstance(msg, (DataStream1Message, DataStream2Message)) and \
+                msg.header.object_ref == object_ref:
+            for e in msg.elements:
+                if (e.item_handle == item_handle) and \
+                        isinstance(e.value, list):
+                    gui.set_ydata(e.value)
+                    break
+            time.sleep(.5)  # give the plot time to update
 
 
-thread = Thread(target=run, daemon=True)
+# the main program loop
+with Thread(target=run, daemon=True) as thread:
 
-# start the GUI
-gui.show()
+    # start the GUI
+    gui.show()
 
-# start the pcap-reading thread
-thread.start()
+    # start the pcap-reading thread
+    thread.start()
 
-# this is the event loop, which is responsible for the GUI.
-# it is run in the main thread
-app.exec()
+    # this is the event loop, which is responsible for the GUI.
+    # it is run in the main thread
+    app.exec()
